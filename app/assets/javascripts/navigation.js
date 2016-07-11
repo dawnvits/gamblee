2bcdddd86d0308df98f339246fb5135faea0eed2
Don Vito Alzaga <dev.dawn024@gmail.com>
2016-07-11 10:25:32 +0800

    Add test case when logging in

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,4 +1,4 @@
-jQuery(document).on('turbolinks:load', function() {
+njQuery(document).on('turbolinks:load', function() {
   jQuery('#navigation').on("click", function() {
     jQuery('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
   });
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  describe 'GET #index' do
-  	before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      sign_in user
-    end
+  describe 'Accessing Homepage' do
+  	context 'when authenticated' do
+  	  before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        user = FactoryGirl.create(:user)
+        sign_in user
+      end
 
-    it 'returns http success when user is logged in' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+      it 'returns http success when user is logged in' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+  	end
+
+  	context 'when not authenticated' do
+  	  it 'it redirects to login page' do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+  	end
   end
 end
