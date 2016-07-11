a8bb01fda3b6d4acc255314d25745561e294f14a
Don Vito Alzaga <dev.dawn024@gmail.com>
2016-07-11 10:59:20 +0800

    Fixed typo

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,4 +1,4 @@
-njQuery(document).on('turbolinks:load', function() {
+jQuery(document).on('turbolinks:load', function() {
   jQuery('#navigation').on("click", function() {
     jQuery('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
   });
