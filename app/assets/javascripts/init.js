64f0e803ffb340bd3049dabc05a70e89bec37538
Don Vito Alzaga <vito@houseme.space>
2016-07-15 00:34:29 +0800

    Update UI

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,4 +2,10 @@ jQuery(document).on('turbolinks:load', function() {
   jQuery('#navigation').on("click", function() {
     jQuery('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
   });
+  
+  jQuery('.ui.checkbox').checkbox();
+  
+  jQuery('.message .close').on('click', function() {
+    jQuery(this).closest('.message').transition('fade');
+  });
 });
