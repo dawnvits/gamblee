a4362ceb032da608087cc4036ff45eb8642e54ad
Don Vito Alzaga <dev.dawn024@gmail.com>
2016-08-16 15:19:34 +0800

    Added activeadmin

---
+++ app/admin/game.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Game do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :schedule, :minutes_for_betting, :description, :winner_determined, :winning_number
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'ipn/new'
 
  devise_for :users,  path_names: { sign_in: 'welcome', sign_up: 'register' }, controllers: { sessions: 'users/sessions',  omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
