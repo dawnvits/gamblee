ce605e39f8919026609b1463664571467f50be06
Don Vito Alzaga <dev.dawn024@gmail.com>
2016-08-19 13:50:54 +0800

    UI updates

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,6 +1,10 @@
 jQuery(document).on('turbolinks:load', function() {
   jQuery('#navigation').on("click", function() {
-    jQuery('.ui.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
+    jQuery('#sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
+  });
+
+  jQuery('#bottom_menu-trigger').on("click", function() {
+    jQuery('#bottom_menu').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
   });
   
   jQuery('.ui.checkbox').checkbox();
